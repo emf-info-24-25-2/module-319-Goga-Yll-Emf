3730e2e1a03860abfb1c834b70b126ed23cd1d76
Goga-Yll_Emf <yll.goga@studentfr.ch>
2024-08-29 11:43:26 +0200

    mon premier commit ex3

---
+++ exercices/exercice03/TypesDeBase.java
@@ -0,0 +1,8 @@
+public class TypesDeBase {
+    public static void main(String[] args) {
+        int age = 18;
+        String nom = "Yll";
+
+        System.out.println("Je m’appelle" +  nom  + "et j’ai" +  age  + "ans");
+    }
+}
