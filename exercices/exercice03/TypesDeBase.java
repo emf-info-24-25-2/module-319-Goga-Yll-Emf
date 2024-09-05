147b4192e6a901b1b5af7c65a0afbb4bdc11b881
Goga-Yll_Emf <yll.goga@studentfr.ch>
2024-09-05 09:28:59 +0200

    j'ai fini l'ex 3

--- exercices/exercice03/TypesDeBase.java
+++ exercices/exercice03/TypesDeBase.java
@@ -1,8 +1,10 @@
 public class TypesDeBase {
     public static void main(String[] args) {
-        int age = 18;
+        int age = 15;
         String nom = "Yll";
 
-        System.out.println("Je m’appelle" +  nom  + "et j’ai" +  age  + "ans");
+        System.out.println("Je m'appelle " +  nom  + " et j'ai " +  age  + " ans");
+        age += 10;
+        System.out.println("dans 10 ans j'aurai " + age + " ans ");
     }
 }
