d18ffd4ab0a0743a8f198781843b8c127a37afec
Goga-Yll_Emf <yll.goga@studentfr.ch>
2024-09-10 16:18:15 +0200

    MON COMMIT

--- devoirs/devoir01/devoir01.java
+++ devoirs/devoir01/devoir01.java
@@ -2,8 +2,9 @@ public class devoir01 {
     public static void main(String[] args) {
         int monAge = 20;
         int estMajeur = 18;
-       
-        System.out.println();
+        
+
+        System.out.println("je m'appelle Yll Goga");
         if (monAge >= estMajeur){
             System.out.println("Je suis majeur");
     }   else{System.out.println("Je ne suis pas encore majeur");
