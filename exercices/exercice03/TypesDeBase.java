4573717378ad0ccf1166597fcf3386ac0cb08291
Goga-Yll-Emf <yll.goga@studentfr.ch>
2024-10-10 11:54:52 +0200

    fin fusion

--- exercices/exercice03/TypesDeBase.java
+++ exercices/exercice03/TypesDeBase.java
@@ -1,10 +1,7 @@
-public class TypesDeBase {
-    public static void main(String[] args) {
-        int age = 15;
-        String nom = "Yll";
+public class TypesDeBase{
 
-        System.out.println("Je m'appelle " +  nom  + " et j'ai " +  age  + " ans");
-        age += 10;
-        System.out.println("dans 10 ans j'aurai " + age + " ans ");
+    public static void main(String[] args) {
+        int age = 0;
+        
     }
 }
