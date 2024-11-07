705d6017d4778c1ab7a00cd3ede1cc13fb9871fa
Goga-Yll_Emf <yll.goga@studentfr.ch>
2024-11-07 11:45:14 +0100

    devoir6 a terminer

---
+++ devoirs/devoir06/devoir6.java
@@ -0,0 +1,24 @@
+public class devoir6 {
+
+    static final int TAILLE_TABLEAU = 20;
+    static final int VALEUR_MIN = 0;
+    static final int VALEUR_MAX = 50;
+    static final int VALEUR_RECHERCHEE = 7;
+
+
+    public static int[] genereTableau(int min,int max,int taille){
+        int[] tableau = new int[taille];
+        for (int i = 0; i < tableau.length; i++) {
+        tableau [i] = (VALEUR_MIN & VALEUR_MAX);
+        }
+
+        return tableau;
+    }
+    public static int rechercheMin()
+
+    public static void main(String[] args) {
+    int[] tableau = genereTableau(TAILLE_TABLEAU, VALEUR_MIN, VALEUR_MAX);
+
+    }
+}
+        
